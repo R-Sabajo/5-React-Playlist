870fe13dd622d67f53a74cb39bea4319887e296f
Rama Sabajo <rsabajo@gmail.com>
2020-09-07 16:23:06 +0200

    commit build for use on github pages

---
+++ mysongs/build/precache-manifest.41a873dbc1b904141858c9ced37c8250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65d5255e61dbbfee32c3b53bb1110ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb83f87e8d31cc33231e",
+    "url": "/static/css/main.b2a16467.chunk.css"
+  },
+  {
+    "revision": "d3e372b4c46c17378063",
+    "url": "/static/js/2.da0a2088.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da0a2088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb83f87e8d31cc33231e",
+    "url": "/static/js/main.ba3292f6.chunk.js"
+  },
+  {
+    "revision": "92e156be8bb89a1f818b",
+    "url": "/static/js/runtime-main.5713e562.js"
+  },
+  {
+    "revision": "8b6488105a0bed7b29d987cee9b098f0",
+    "url": "/static/media/tapes.8b648810.jpg"
+  }
+]);
